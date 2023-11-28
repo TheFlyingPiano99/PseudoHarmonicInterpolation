449dde122fe719c399ebc9c98aa72670ebdc05c0
TheFlyingPiano99 <simonzolika1999@gmail.com>
2023-11-28 21:50:28 +0100

    Initial commit

---
+++ PseudoHarmonicInterpolation/PseudoHarmonicInterpolation.cpp
@@ -0,0 +1,12 @@
+﻿// PseudoHarmonicInterpolation.cpp : Defines the entry point for the application.
+//
+
+#include "PseudoHarmonicInterpolation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
